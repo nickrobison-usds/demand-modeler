9c1a21f653a94fb2ecb8e5bc48d9295fb3811d80
Tim Best <timothy.best@hq.dhs.gov>
2020-03-17 13:18:08 -0400

    add and run prettier

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import Header from "./components/Header/Header";
-import {AppStoreProvider} from "./app/AppStore";
+import { AppStoreProvider } from "./app/AppStore";
 import CaseTable from "./components/CaseTable";
 import EpiMap from "./components/EpiMap";
 
 function App() {
-
-    return (
-        <AppStoreProvider>
-            <div className="App">
-                <Header title="Demand Modeller" titleRoute="/"/>
-                <EpiMap/>
-                <CaseTable/>
-            </div>
-        </AppStoreProvider>
-    );
+  return (
+    <AppStoreProvider>
+      <div className="App">
+        <Header title="Demand Modeller" titleRoute="/" />
+        <EpiMap />
+        <CaseTable />
+      </div>
+    </AppStoreProvider>
+  );
 }
 
 export default App;
