16acd05d81d003b89eb6dadfbcb50abcbdaee7dc
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-03-16 21:01:51 -0400

    Initial commit of Frontend
    Added React app frontend and necessary routing to display it

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nickrobison-usds/demand-modeling
 
 go 1.13
 
-require github.com/jackc/pgx/v4 v4.5.0
+require (
+	github.com/go-chi/chi v4.0.3+incompatible
+	github.com/jackc/pgx/v4 v4.5.0
+)
--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"github.com/jackc/pgx/v4"
 	"github.com/nickrobison-usds/demand-modeling/cmd"
 	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
@@ -21,4 +27,41 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "ui/build")
+	err = serve(filesDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func rootHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	}
+}
+
+func serve(filesDir string) error {
+	r := chi.NewRouter()
+
+	r.Get("/api", rootHandler())
+	FileServer(r, "", "/", http.Dir(filesDir))
+
+	return http.ListenAndServe(":8080", r)
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, basePath string, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+	fs := http.StripPrefix(basePath+path, http.FileServer(root))
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	})
 }
