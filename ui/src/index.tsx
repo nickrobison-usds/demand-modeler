9ccfb9c86e895482ca0dffd60ccd9554fbadb00e
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-03-16 21:39:42 -0400

    Added USWDS to create a really simple landing page.
    SCSS and JS are loaded to spit out an ugly header.

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "uswds";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
