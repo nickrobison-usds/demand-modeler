fd80de5b781f7061ff5dda2d3b6ab0f0ccd0a095
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-03-17 11:22:23 -0400

    Labels and hover events are working.

--- ui/src/components/EpiMap.tsx
+++ ui/src/components/EpiMap.tsx
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useState} from "react";
 import {ActionType, AppContext} from "../app/AppStore";
-import ReactMapGL, {Layer, Source} from "react-map-gl";
+import ReactMapGL, {Layer, Source, PointerEvent} from "react-map-gl";
 import {range} from 'd3-array';
 import {scaleQuantile} from 'd3-scale';
 
@@ -25,6 +25,35 @@ const dataLayer = {
         'fill-opacity': 0.8,
         'fill-outline-color': 'white'
     }
+};
+
+const labels = {
+    id: "county-name",
+    type: "symbol",
+    source: "data",
+    layout: {
+        "text-field": "{name}\n",
+        // "text-font": ["Droid Sans Regular"],
+        "text-size": 12,
+        // 'symbol-placement': 'point'
+    },
+    paint: {
+        "text-color": "black"
+    }
+    // paint: {
+    //     "text-color": ["case",
+    //         ["boolean", ["feature-state", "hover"], false],
+    //         'rgba(255,0,0,0.75)',
+    //         'rgba(0,0,0,0.75)'
+    //     ],
+    //     "text-halo-color": ["case",
+    //         ["boolean", ["feature-state", "hover"], false],
+    //         'rgba(255,255,0,0.75)',
+    //         'rgba(255,255,255,0.75)'
+    //     ],
+    //     "text-halo-width": 2,
+    //     "text-halo-blur": 0,
+    // }
 };
 
 function updatePercentiles(featureCollection: GeoJSON.FeatureCollection, accessor: ((f: GeoJSON.Feature) => number)): GeoJSON.FeatureCollection {
@@ -66,7 +95,8 @@ const EpiMap: React.FunctionComponent = () => {
                 type: "Feature",
                 geometry: value.Geo,
                 properties: {
-                    cases: value.Confirmed
+                    cases: value.Confirmed,
+                    name: `${value.County}, ${value.State}`
                 }
             }
         });
@@ -75,6 +105,15 @@ const EpiMap: React.FunctionComponent = () => {
     const accessor = (f: GeoJSON.Feature): number => {
         return f.properties?.cases;
     };
+
+    const onHover = (event: PointerEvent) => {
+        const {
+            features
+        } = event;
+        const hoveredFeature = features && features.find(f => f.layer.id === 'data');
+        console.log("Hovered over: ", hoveredFeature);
+    };
+
 
     useEffect(() => {
         console.debug("Effect cases: ", cases);
@@ -94,9 +133,11 @@ const EpiMap: React.FunctionComponent = () => {
                     mapboxApiAccessToken="pk.eyJ1Ijoibmlja3JvYmlzb24tdXNkcyIsImEiOiJjazd2djdpenkwZmxxM2ZwNWV0NHJpMWVvIn0.ccAlySc6edOpNYfHci-6Aw"
                     onViewportChange={v => {
                         dispatch({type: ActionType.UPDATE_MAPVIEW, payload: v});
-                    }}>
+                    }}
+        onHover={e => onHover(e)}>
             <Source id="data" type="geojson" data={data}>
                 <Layer {...dataLayer}/>
+                <Layer {...labels}/>
             </Source>
         </ReactMapGL>
     )
