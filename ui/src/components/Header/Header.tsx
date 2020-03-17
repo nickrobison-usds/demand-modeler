cb1e7cffdad318cf4a3951ec86304c8c040d43bc
Tim Best <timothy.best@hq.dhs.gov>
2020-03-17 13:15:39 -0400

    remove boilerplate

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -20,76 +20,7 @@ const Header: React.FunctionComponent<HeaderProps> = ({
               </a>
             </em>
           </div>
-          <button className="usa-menu-btn">Menu</button>
         </div>
-        <nav aria-label="Primary navigation" className="usa-nav">
-          <button className="usa-nav__close">
-            <img src="/assets/img/close.svg" alt="close" />
-          </button>
-          <ul className="usa-nav__primary usa-accordion">
-            <li className="usa-nav__primary-item">
-              <button
-                className="usa-accordion__button usa-nav__link  usa-current"
-                aria-expanded="false"
-                aria-controls="basic-nav-section-one"
-              >
-                <span>Current section</span>
-              </button>
-              <ul id="basic-nav-section-one" className="usa-nav__submenu">
-                <li className="usa-nav__submenu-item">
-                  <a href="#">Navigation link</a>
-                </li>
-                <li className="usa-nav__submenu-item">
-                  <a href="#">Navigation link</a>
-                </li>
-                <li className="usa-nav__submenu-item">
-                  <a href="#">Navigation link</a>
-                </li>
-              </ul>
-            </li>
-            <li className="usa-nav__primary-item">
-              <button
-                className="usa-accordion__button usa-nav__link"
-                aria-expanded="false"
-                aria-controls="basic-nav-section-two"
-              >
-                <span>Section</span>
-              </button>
-              <ul id="basic-nav-section-two" className="usa-nav__submenu">
-                <li className="usa-nav__submenu-item">
-                  <a href="#">Navigation link</a>
-                </li>
-                <li className="usa-nav__submenu-item">
-                  <a href="#">Navigation link</a>
-                </li>
-                <li className="usa-nav__submenu-item">
-                  <a href="#">Navigation link</a>
-                </li>
-              </ul>
-            </li>
-            <li className="usa-nav__primary-item">
-              <a className="usa-nav__link" href="javascript:void(0)">
-                <span>Simple link</span>
-              </a>
-            </li>
-          </ul>
-          <form className="usa-search usa-search--small ">
-            <div role="search">
-              <label className="usa-sr-only" htmlFor="basic-search-field-small">
-                Search small
-              </label>
-              <input
-                className="usa-input"
-                id="basic-search-field-small"
-                type="search"
-                name="search"
-              />
-              <button className="usa-button" type="submit">
-                <span className="usa-sr-only">Search</span>
-              </button>
-            </div>
-          </form>
-        </nav>
       </div>
     </header>
   );
