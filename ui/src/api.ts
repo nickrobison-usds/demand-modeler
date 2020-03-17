cb379df6674d003401ff035662d56c913f6cdb35
Tim Best <timothy.best@hq.dhs.gov>
2020-03-17 14:10:36 -0400

    cleanup api url config

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,10 +1,9 @@
 import { Case } from "./app/AppStore";
-import { config } from "./config";
 import URI from "urijs";
 import Axios from "axios";
 
 export async function fetchCaseCounts(): Promise<Case[]> {
-  const url = URI(`${config.API_URI}/api/cases`);
+  const url = URI(`${process.env.REACT_APP_API_URI}/api/cases`);
   const resp = await Axios.get<Case[]>(url.readable());
   return resp.data;
 }
