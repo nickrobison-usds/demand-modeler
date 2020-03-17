1a97c4c516732b6fa4b82917eb2c3ad611c68c87
Tim Best <timothy.best@hq.dhs.gov>
2020-03-17 14:04:26 -0400

    remove dead code

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import { AppStoreProvider } from "./app/AppStore";
-import CaseTable from "./components/CaseTable";
 import EpiMap from "./components/EpiMap";
 
 function App() {
   return (
     <AppStoreProvider>
-      <div className="App">
-        <Header title="Demand Modeller" titleRoute="/" />
-        <EpiMap />
-        <CaseTable />
-      </div>
+      <Header title="Demand Modeller" titleRoute="/" />
+      <EpiMap />
     </AppStoreProvider>
   );
 }
