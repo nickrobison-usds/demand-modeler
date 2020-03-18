a75dcbbdcbebfab0fb05bf5db8e47aa15a99db65
Tim Best <timothy.best@hq.dhs.gov>
2020-03-17 22:17:23 -0400

    update state and add mock

--- ui/src/utils/DateUtils.ts
+++ ui/src/utils/DateUtils.ts
@@ -1,3 +1,3 @@
 export function formatDate(date: Date): string {
-    return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
 }
