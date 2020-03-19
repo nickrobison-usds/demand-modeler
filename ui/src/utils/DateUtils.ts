ec140399a3f0eb43db18c72aaff3e6882406729b
Tim Best <timothy.best@hq.dhs.gov>
2020-03-19 10:53:30 -0400

    remove year

--- ui/src/utils/DateUtils.ts
+++ ui/src/utils/DateUtils.ts
@@ -1,5 +1,5 @@
 export function formatDate(date: Date): string {
-  return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+  return `${date.getMonth()+1}-${date.getDate()}`;
 }
 
 export function parseDate(date: string): Date {
