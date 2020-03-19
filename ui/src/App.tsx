ed45f5bb8aa50bd45fd113d2a2258a88dc6c57bc
Nicholas Scialli <nicholas.a.scialli@uscis.dhs.gov>
2020-03-18 20:53:48 -0400

    Fix report and cleanup charts

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -13,11 +13,11 @@ import { Report } from "./components/Charts/Report";
 function App() {
   const url = new URL(window.location.href);
   const report = url.searchParams.get("report");
-  console.log(report)
+  console.log(report);
   if (report) {
     return (
       <AppStoreProvider>
-        <Report/>
+        <Report />
       </AppStoreProvider>
     );
   } else {
@@ -34,23 +34,21 @@ function App() {
             {/* <EpiMap stat="confirmed" /> */}
             {/* <USATotals stat="confirmed" /> */}
             <ChartContainer2 stat="confirmed" />
-            <ChartContainer stat="confirmed" stateCount={true}/>
-            <ChartContainer stat="confirmed" stateCount={false}/>
+            <ChartContainer stat="confirmed" stateCount={true} />
+            <ChartContainer stat="confirmed" stateCount={false} />
           </Card>
           <Card header="Dead">
             {/* <EpiMap stat="dead" /> */}
             {/* <USATotals stat="dead" /> */}
             <ChartContainer2 stat="dead" />
-            <ChartContainer stat="dead" stateCount={true}/>
-            <ChartContainer stat="dead" stateCount={false}/>
+            <ChartContainer stat="dead" stateCount={true} />
+            <ChartContainer stat="dead" stateCount={false} />
           </Card>
         </div>
-        <div>
-        </div>
+        <div></div>
       </AppStoreProvider>
     );
   }
-
 }
 
 export default App;
