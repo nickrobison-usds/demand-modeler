56dc1cdcc37289a8ea94968def306f04e43b754f
Tim Best <timothy.best@hq.dhs.gov>
2020-03-19 11:42:07 -0400

    update report

--- ui/src/components/Charts/Report.tsx
+++ ui/src/components/Charts/Report.tsx
@@ -3,9 +3,34 @@ import { AppContext } from "../../app/AppStore";
 import { StateMixedBar } from "./StateMixedBar";
 import { MixedBar } from "./MixedBar";
 import "./Report.scss";
-import { formatDate } from "../../utils/DateUtils";
+import { formatDate, dateTimeString } from "../../utils/DateUtils";
 
 export const Report: React.FC<{}> = () => {
+  const LAST_UPDATED = new Date("22:00 March 17, 2020")
+
+  const pagebreak = () => {
+    return (
+      <div style={{margin: "20px 0", fontSize:"13px"}}>
+        <div>
+          Source:{" "}
+          <a
+            href="https://www.csbs.org/information-covid-19-coronavirus"
+            target="_blank"
+          >
+            Conference of State Bank Supervisors
+          </a>
+          , as of {dateTimeString(LAST_UPDATED)}.
+          {/* 12 states with highest case count as of 3/17 shown. */}
+        </div>
+        <div>
+          Data sourced from state health department websites; reporting may be
+          incomplete or delayed
+        </div>
+        <div className="pagebreak" />
+      </div>
+    );
+  };
+
   return (
     <AppContext.Consumer>
       {({ state }) => {
@@ -16,7 +41,7 @@ export const Report: React.FC<{}> = () => {
           .sort((s1, s2) => s2.Confirmed - s1.Confirmed)
           .slice(0, 10);
         return (
-          <div className="report">
+          <div className="report grid-container" style={{marginLeft: 0}}>
             {top10States.map(s => (
               <>
                 <StateMixedBar
@@ -27,7 +52,7 @@ export const Report: React.FC<{}> = () => {
                   stateCount={false}
                   reportView
                 />
-                <div className="pagebreak" />
+                {pagebreak()}
               </>
             ))}
             <StateMixedBar
@@ -38,7 +63,7 @@ export const Report: React.FC<{}> = () => {
               stateCount={false}
               reportView
             />
-            <div className="pagebreak" />
+            {pagebreak()}
             <StateMixedBar
               state={undefined}
               county={undefined}
@@ -47,7 +72,7 @@ export const Report: React.FC<{}> = () => {
               stateCount={true}
               reportView
             />
-            <div className="pagebreak" />
+            {pagebreak()}
             <MixedBar
               state={undefined}
               county={undefined}
--- ui/src/utils/DateUtils.ts
+++ ui/src/utils/DateUtils.ts
@@ -11,5 +11,25 @@ export function getPreviousDate(date: string): string {
   const d = parseDate(date);
   d.setDate(d.getDate() - 1)
   return formatDate(d);
+}
 
+const theMonths = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const addZero = (i: number) => {
+  if (i < 10) {
+    return `0${i}`;
+  }
+  return i;
+}
+
+export const dateTimeString = (date: Date) => {
+  const hour = addZero(date.getHours());
+  const minute = addZero(date.getMinutes());
+  const month = theMonths[date.getMonth()];
+  const day = date.getDay();
+  const year = date.getFullYear()
+  return `${hour}:${minute} ${month} ${day}, ${year}`;
 }
