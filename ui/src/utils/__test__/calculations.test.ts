b2aaa0c6e2c74566c1acae8c354e8e3c26d6836a
Nick Scialli <49658917+nickscialli-usds@users.noreply.github.com>
2020-03-21 04:53:43 -0400

    Isolate math functions (#65)
    * Add mock state function
    
    * Add more mock data
    
    * Initial function
    
    * Add tests
    
    * More tests
    
    * Add county grand total
    
    * Add tests for county grand total
    
    * Make equal values sort by county/state name
    
    Co-authored-by: Nicholas Scialli <nicholas.a.scialli@uscis.dhs.gov>

---
+++ ui/src/utils/__test__/calculations.test.ts
@@ -0,0 +1,83 @@
+import { createMockState } from "../../app/mockState";
+import {
+  getTopCounties,
+  getTopStates,
+  getCountyGrandTotal
+} from "../calculations";
+import {
+  top10ConfirmedByState,
+  top10DeadByState,
+  top10Overall,
+  top10DeadOverall,
+  top10StatesConfirmed,
+  countyGrandTotalForState,
+  countyGrandTotalForCounty,
+  top10StatesDead
+} from "./matchers";
+
+describe("calculations", () => {
+  describe("top 10 counties in state", () => {
+    it("should return top 10 counties in state (confirmed)", () => {
+      const state = createMockState({ date: "2020-03-19", state: "03" });
+      const top10 = getTopCounties(state, "confirmed");
+      expect(top10.length).toEqual(10);
+      expect(top10).toEqual(top10ConfirmedByState);
+    });
+    it("should return top 5 counties in state (confirmed)", () => {
+      const state = createMockState({ date: "2020-03-19", state: "03" });
+      const top10 = getTopCounties(state, "confirmed", 5);
+      expect(top10).toEqual(top10ConfirmedByState.slice(0, 5));
+    });
+    it("should return top 10 counties in state (dead)", () => {
+      const state = createMockState({ date: "2020-03-19", state: "03" });
+      const top10 = getTopCounties(state, "dead");
+      expect(top10).toEqual(top10DeadByState);
+    });
+    it("should return top 5 counties in state (dead)", () => {
+      const state = createMockState({ date: "2020-03-19", state: "03" });
+      const top10 = getTopCounties(state, "dead", 5);
+      expect(top10).toEqual(top10DeadByState.slice(0, 5));
+    });
+  });
+  describe("top 10 counties overall", () => {
+    it("should return top 10 counties overall (confirmed)", () => {
+      const state = createMockState({ date: "2020-03-19" });
+      const top10 = getTopCounties(state, "confirmed");
+      expect(top10).toEqual(top10Overall);
+    });
+    it("should return top 10 counties overall (dead)", () => {
+      const state = createMockState({ date: "2020-03-19" });
+      const top10 = getTopCounties(state, "dead");
+      expect(top10).toEqual(top10DeadOverall);
+    });
+  });
+  describe("top 10 counties in state", () => {
+    it("should get the top 10 states (confirmed)", () => {
+      const state = createMockState({ date: "2020-03-19" });
+      const top10 = getTopStates(state, "confirmed");
+      expect(top10).toEqual(top10StatesConfirmed);
+    });
+    it("should get the top 10 states (dead)", () => {
+      const state = createMockState({ date: "2020-03-19" });
+      const top10 = getTopStates(state, "dead");
+      expect(top10).toEqual(top10StatesDead);
+    });
+  });
+
+  describe("county grand totals", () => {
+    it("calculates the county-based grand totals for a state", () => {
+      const state = createMockState({ date: "2020-03-19", state: "03" });
+      const grandTotal = getCountyGrandTotal(state);
+      expect(grandTotal).toEqual(countyGrandTotalForState);
+    });
+    it("calculates the county-based grand totals for a county", () => {
+      const state = createMockState({
+        date: "2020-03-19",
+        state: "03",
+        county: "03002"
+      });
+      const grandTotal = getCountyGrandTotal(state);
+      expect(grandTotal).toEqual(countyGrandTotalForCounty);
+    });
+  });
+});
---
+++ ui/src/utils/calculations.ts
@@ -0,0 +1,143 @@
+import { AppState, County, State } from "../app/AppStore";
+
+type GrandTotal = {
+  [d: string]: {
+    Confirmed: number;
+    NewConfirmed: number;
+    Dead: number;
+    NewDead: number;
+  };
+};
+
+const getSelectedStateName = (state: AppState) => {
+  if (state.selection.state === undefined) {
+    return null;
+  }
+  return state.covidTimeSeries.states[state.selection.state][0].State;
+};
+
+export const getTopCounties = (
+  appState: AppState,
+  stat: "confirmed" | "dead",
+  numberOfCounties = 10
+): County[][] => {
+  const stateName = getSelectedStateName(appState);
+  let counties = Object.values(appState.covidTimeSeries.counties).reduce(
+    (acc, el) => [...acc, ...el],
+    []
+  );
+  // If a state is selected, limit to that state
+  if (stateName) {
+    counties = counties.filter(({ State }) => State === stateName);
+  }
+  // Get latest date for sorting purposes
+  const latestDate = counties.reduce(
+    (acc, el) => (el.Reported > acc ? el.Reported : acc),
+    counties[0].Reported
+  );
+  // Prevent possible duplicates and sort
+  const seenCounties = new Set<string>();
+  const newestCounties = counties
+    .filter(county => {
+      if (
+        county.Reported.toLocaleDateString() !==
+          latestDate.toLocaleDateString() ||
+        seenCounties.has(county.ID)
+      ) {
+        return false;
+      }
+      seenCounties.add(county.ID);
+      return true;
+    })
+    .sort((a, b) => {
+      const metric = stat === "confirmed" ? "Confirmed" : "Dead";
+      if (b[metric] === a[metric]) {
+        return b.County > a.County ? 1 : -1;
+      }
+      return b[metric] > a[metric] ? 1 : -1;
+    });
+  const topCounties = newestCounties
+    .slice(0, numberOfCounties)
+    .map(county => appState.covidTimeSeries.counties[county.ID]);
+
+  return topCounties;
+};
+
+export const getTopStates = (
+  appState: AppState,
+  stat: "confirmed" | "dead",
+  numberOfCounties = 10
+): State[][] => {
+  let states = Object.values(appState.covidTimeSeries.states).reduce(
+    (acc, el) => [...acc, ...el],
+    []
+  );
+  // Get latest date for sorting purposes
+  const latestDate = states.reduce(
+    (acc, el) => (el.Reported > acc ? el.Reported : acc),
+    states[0].Reported
+  );
+  // Prevent possible duplicates and sort
+  const seenStates = new Set<string>();
+  const newestStateData = states
+    .filter(state => {
+      if (
+        state.Reported.toLocaleDateString() !==
+          latestDate.toLocaleDateString() ||
+        seenStates.has(state.ID)
+      ) {
+        return false;
+      }
+      seenStates.add(state.ID);
+      return true;
+    })
+    .sort((a, b) => {
+      const metric = stat === "confirmed" ? "Confirmed" : "Dead";
+      if (b[metric] === a[metric]) {
+        return b.State > a.State ? 1 : -1;
+      }
+      return b[metric] > a[metric] ? 1 : -1;
+    });
+  const topStates = newestStateData
+    .slice(0, numberOfCounties)
+    .map(state => appState.covidTimeSeries.states[state.ID]);
+
+  return topStates;
+};
+
+export const getCountyGrandTotal = (appState: AppState): GrandTotal => {
+  let counties = Object.values(appState.covidTimeSeries.counties).reduce(
+    (acc, el) => [...acc, ...el],
+    []
+  );
+
+  if (appState.selection.county) {
+    counties = counties.filter(
+      county => county.ID === appState.selection.county
+    );
+  } else if (appState.selection.state) {
+    const selectedState = getSelectedStateName(appState);
+    counties = counties.filter(county => county.State === selectedState);
+  }
+
+  const byDate = counties.reduce((acc, el) => {
+    const key = el.Reported.toLocaleDateString();
+    if (!acc[key]) {
+      acc[key] = {
+        Confirmed: 0,
+        NewConfirmed: 0,
+        Dead: 0,
+        NewDead: 0
+      };
+    }
+    acc[key] = {
+      Confirmed: acc[key].Confirmed + el.Confirmed,
+      NewConfirmed: acc[key].NewConfirmed + el.NewConfirmed,
+      Dead: acc[key].Dead + el.Dead,
+      NewDead: acc[key].NewDead + el.NewDead
+    };
+    return acc;
+  }, {} as GrandTotal);
+
+  return byDate;
+};
