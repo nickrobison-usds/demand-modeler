f5531ac7e177b7fe8bdfe5d13a13087d02bef38b
Lisa Chung <lisachung314@gmail.com>
2020-03-22 23:06:15 -0400

    add function that will find the uva data files

--- data/uva/compile_uva_county_data.py
+++ data/uva/compile_uva_county_data.py
@@ -1,9 +1,29 @@
 from datetime import datetime
+import os
+import re
+
+UVA_DATA_FILES_REGEX = r'^uva-county_[0-9]{4}-[0-9]{2}-[0-9]{2}.csv'
 
 def create_file(filename):
 	with open(filename, 'w') as f:
 		f.write('countyName, stateName, confirmed, lastUpdate')
 
+def get_data_files():
+	files = [f for f in os.listdir('.') if re.match(UVA_DATA_FILES_REGEX, f)]
+	return files
+
+
+def run_tests():
+	files = get_data_files()
+	assert len(files) == 5
+	assert 'uva-county_2020-03-03.csv' in files 
+	assert 'uva-county_2020-03-07.csv' in files 
+
+
+
 if __name__ == "__main__":
-    filename = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv"
-    create_file(filename)
+	run_tests()
+	
+	filename = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv"
+	create_file(filename)
+	
