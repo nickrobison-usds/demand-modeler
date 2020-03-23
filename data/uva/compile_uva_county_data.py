7f0c55afc443924acb5929c5b5ced2f904430936
Lisa Chung <lisachung314@gmail.com>
2020-03-22 22:54:31 -0400

    add a function that creates a csv file of 4 columns

--- data/uva/compile_uva_county_data.py
+++ data/uva/compile_uva_county_data.py
@@ -1,5 +1,9 @@
-def main():
-    print("Hello World!")
+from datetime import datetime
+
+def create_file(filename):
+	with open(filename, 'w') as f:
+		f.write('countyName, stateName, confirmed, lastUpdate')
 
 if __name__ == "__main__":
-    main()
+    filename = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv"
+    create_file(filename)
