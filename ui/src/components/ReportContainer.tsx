14c54dc0b713da657372e59dfccf23a591847acf
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-03-27 15:25:40 -0400

    Add title page to slides.

--- ui/src/components/ReportContainer.tsx
+++ ui/src/components/ReportContainer.tsx
@@ -10,8 +10,50 @@ export const ReportContainer: React.FC = props => {
         const ppt = new pptxgen();
         ppt.layout = "LAYOUT_16x9";
         ppt.company = "United States Digital Service";
-        const slide = ppt.addSlide();
-        slide.addText("Hello, from PowerPoint");
+        // Generate the title slide
+        const titleSlide = ppt.addSlide();
+        titleSlide.addText("COVID-19 county-level case data", {
+            fontFace: 'Calibri (Headings)',
+            bold: true,
+            color: ppt.SchemeColor.text2,
+            x: 1.8,
+            y: 2.26,
+            fontSize: 40,
+            h: 0.38,
+            w: 8.2
+        });
+        titleSlide.addText("Data as of (today)", {
+            fontSize: 20,
+            x: 1.8,
+            y: 2.75
+        });
+        titleSlide.addShape(ppt.ShapeType.line, {
+            color: "#FF0000",
+            height: 3.17,
+            width: 0.03,
+            rotate: 45,
+            x: .31,
+            y: 1.25,
+            h: 2.84,
+            w: 2.85
+        });
+
+        // Add the map
+        let map = document.getElementsByClassName("mapboxgl-map").item(0);
+
+        console.debug("Map element: ", map);
+
+        const url = await domtoimage.toPng(map!);
+        console.debug("Map URL: ", url);
+        const mapSlide = ppt.addSlide();
+        mapSlide.addImage({
+            data: url,
+            // These positioning values are hard coded based on manual viewing and aligning of the graphs.
+            h: 5.6,
+            w: 5.6,
+            x: 1.9
+        });
+
 
         const svgElements = document.getElementsByClassName("report-chart");
         for (let element of svgElements[Symbol.iterator]()) {
