8db7a546097c89e2708c74ce11969734e3780f53
Ron Heft <52176593+ronaldheft-usds@users.noreply.github.com>
2020-04-01 18:47:07 -0400

    Fix import issue due to rebase (#141)
    Co-authored-by: Tim Best <53869143+timothybest-usds@users.noreply.github.com>

--- ui/src/components/ReportContainer.tsx
+++ ui/src/components/ReportContainer.tsx
@@ -93,7 +93,7 @@ export const addTopTenStates = (
 
   console.debug("Data Combined:", dataCombined);
 
-  PowerPointUtils.addClusteredStackedChart(s, dataCombined, {
+  PowerPointUtils.addClusteredStackedChart(ppt, s, dataCombined, {
     x: 1,
     y: 1,
     w: 8,
@@ -109,7 +109,7 @@ export const ReportContainer: React.FC<ReportContainerProps> = props => {
     ppt.company = "United States Digital Service";
     // Generate the title slide
     addTitleSlide(ppt);
-    // addTopTenStates(ppt, props.states, props.weeklyTimeSeries)
+    addTopTenStates(ppt, props.states, props.weeklyTimeSeries);
 
     const TEXT_COLOR = "0A2644";
     const TEXT_FONT_FACE = "Source Sans Pro";
