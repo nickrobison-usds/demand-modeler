e0fc1d5f779648b1f211a10c4dac003451e5222f
Tim Best <53869143+timothybest-usds@users.noreply.github.com>
2020-04-06 19:10:30 -0400

    slide config (#155)

---
+++ ui/src/components/PowerPointGenerator/Slides/CBSASlides/multiCbsaPerSlide.ts
@@ -0,0 +1,46 @@
+export const slides = [
+  //Seattle
+  ["42660"],
+  // San Francisco & San Jose
+  ["41860", "41940"],
+  // Los Angeles & San Bernardino & San Diego
+  ["31080", "40140", "41740"],
+  // Las Vegas
+  ["29780"],
+  // Phoenix
+  ["38060"],
+  // Denver
+  ["19740"],
+  // Dallas & Houston
+  ["19100", "26420"],
+  // New Orleans & Baton Rouge
+  ["35380", "12940"],
+  // Miami & Orlando & Tampa
+  ["33100", "36740", "45300"],
+  // Atlanta & Charlotte
+  ["12060", "16740"],
+  // Nashville & Memphis
+  ["34980", "32820"],
+  // St. Louis & Indianapolis
+  ["41180", "26900"],
+  // Milwaukee & Chicago
+  ["33340", "16980"],
+  // Detroit
+  ["19820"],
+  // Cleveland & Pittsburgh
+  ["17460", "38300"],
+  // Buffalo
+  ["15380"],
+  // Dc & Baltimore
+  ["47900", "12580"],
+  // Philadelphia & Allentown
+  ["37980", "10900"],
+  // NYC
+  ["35620"],
+  // Bridgeport & New haven
+  ["14860", "35300"],
+  // Providence & Worcester
+  ["39300", "49340"],
+  // Boston
+  ["14460"]
+]
