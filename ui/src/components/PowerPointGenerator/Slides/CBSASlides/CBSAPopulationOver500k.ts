b888f076faa8095c2f25e634e7bbabeb378f5c66
Tim Best <timothy.best@hq.dhs.gov>
2020-04-08 00:54:58 -0400

    reverse reverse

--- ui/src/components/PowerPointGenerator/Slides/CBSASlides/CBSAPopulationOver500k.ts
+++ ui/src/components/PowerPointGenerator/Slides/CBSASlides/CBSAPopulationOver500k.ts
@@ -61,6 +61,8 @@ const getChartData = (
     // The first day is only used to calculate diffs. Remove it.
     state.labels.reverse().shift();
     state.values.reverse().shift();
+    state.labels.reverse();
+    state.values.reverse();
     acc.push(state);
     return acc;
   }, [] as ChartData[]);
