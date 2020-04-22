dfdffc6855acc0e1727c985270de7e833260c995
0kl <git@0kl.com>
2020-04-22 01:20:47 -0400

    4/21 update, missing counties in WA and all of Montana

--- data/incoming/update.js
+++ data/incoming/update.js
@@ -3,11 +3,12 @@ const path = require('path');
 const parse = require("csv-parse/lib/sync");
 const writer = require('csv-writer').createObjectCsvWriter
 
-const filename = "covid19_county_2020-04-20.csv";
+const filename = "covid19_county_fips..csv";
 const county = fs.readFileSync(path.join(__dirname, filename));
 
+
 const csvWriter = writer({
-  path: path.join(__dirname,'..', filename),
+  path: path.join(__dirname,'..', "cleaned_"+filename),
   header: [
     { id: "County Name", title: "County Name" },
     { id: "State Name", title: "State Name" },
@@ -28,7 +29,7 @@ const csvWriter = writer({
   ],
 });
 const dupes = {}
-
+const stateNewCases = {};
 const results = parse(county.toString(), {
   columns: true,
   skip_empty_lines: true,
@@ -38,12 +39,22 @@ const results = parse(county.toString(), {
   dupes[key] = true;
   return true;
 }).map((c) => {
+  if (c["State Name"] in stateNewCases) {
+    stateNewCases[c["State Name"]]+= +c['New'];
+  } else {
+    stateNewCases[c['State Name']] = 0;
+  }
   const updated = { ...c };
   if (c["County Name"] == "Walton" && c["State Name"] == "Florida") {
     updated['County Name'] = 'Walton County';
   }
   return updated;
 })
+
+Object.entries(stateNewCases).forEach(state => {
+  if (state[0] === "District of Columbia") return;
+  if (!state[1]) throw new Error("No new cases in " + state[0]);
+});
 
 csvWriter
   .writeRecords(results)
