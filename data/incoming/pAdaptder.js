2484823367e750ba1fec9f8ade682b9eb303bdc7
Kelvin Luu <kelvin.t.luu@omb.eop.gov>
2020-05-05 18:05:52 -0400

    update with usafacts data

--- data/incoming/pAdaptder.js
+++ data/incoming/pAdaptder.js
@@ -11,24 +11,33 @@ const { addCases
     , updatePopulation
     , clientConnected
     , closeConnection} = require('./db');
-const pFile = path.join(__dirname, 'cases_by_county_with_overrides.csv');;
-
+const pFile = path.join(__dirname, 'scratch_data','cases_by_county_with_overrides.csv');
 const counties = {};
 const pAdapterStream = new stream.Writable({
     objectMode: true,
     async write(record, _encoding, callback) {
-        await clientConnected;
-        const date = moment(record['date']).format('YYYY-MM-DD');
-        const fips = record["fips_code"];
-        const sourceKey = Object.keys(record)[0]; // had issues using the key 'source' due to invisible characters. 
-        const source = record[sourceKey].toLowerCase();
+        if (record['unit'] !== 'persons'){ return callback();}
 
+        await clientConnected;
+
+        const date = moment(record['date']).format('YYYY-MM-DD');
+        let fips;
+        
+        const fipsKey = Object.keys(record)[0]; // had issues using the key 'source' due to invisible characters. 
+        // const sourceKey = Object.keys(record)[0]; // had issues using the key 'source' due to invisible characters. 
+
+        switch (record[fipsKey]){
+            case '36061': fips = '36500'; break; // Remap NY
+            default: fips = `${record[fipsKey]}`; break;
+        }
+        const source = record["source"].toLowerCase();
+        
         if (!(fips in counties)) {
             await createCounty(
                 fips
                 , record["County_county"]
                 , record["state_name"]
-                , record["state_fips"]
+                , fips.slice(0,2)
                 , fips.slice(2)
                 , +record['Total_Total_Pop'] / 1000
             );
@@ -42,7 +51,7 @@ const pAdapterStream = new stream.Writable({
             case "deaths": await addDeaths(fips, date, source, record['value'], date); break;
             default: throw new Error(`Invalid record type! ${record['type']}`);
         }
-        callback();
+        return callback();
     }
 });
 pAdapterStream.on("finish", async () => {
