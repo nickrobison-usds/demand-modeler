0bee65e897915d1e7292c4666e2fa38bb8f4ac90
Kelvin Luu <kelvin.t.luu@omb.eop.gov>
2020-05-05 18:26:20 -0400

    update with all USAS data

--- data/incoming/writeCountyData.js
+++ data/incoming/writeCountyData.js
@@ -2,16 +2,16 @@ const path = require('path');
 const moment = require('moment');
 const writer = require("csv-writer").createObjectCsvWriter;
 
-const { clientConnected, closeConnection, getDemandModelerData,getMostRecentDate } = require('./db');
+const { clientConnected, closeConnection, getDemandModelerData, getMostRecentDate } = require('./db');
 
 clientConnected.then(async () => {
     let date = moment(await getMostRecentDate()).format("YYYY-MM-DD");
-    for (var i = 0; i < 14; i++){
-        if (moment(date).isAfter("2020-04-23")){
-            await writeDataForDate("usafacts", date);
-        } else {
-            await writeDataForDate('csbs', date);
-        }
+    for (var i = 0; i < 14; i++) {
+        // if (moment(date).isAfter("2020-04-23")){
+        await writeDataForDate("usafacts", date);
+        // } else {
+        //     await writeDataForDate('csbs', date);
+        // }
         date = moment(date).subtract(1, "days").format("YYYY-MM-DD");
     }
 
@@ -20,17 +20,19 @@ clientConnected.then(async () => {
     console.log(err);
 });
 
-async function writeDataForDate(source,date){
-    const results = (await getDemandModelerData(source, date)).map(r=>{
+async function writeDataForDate(source, date) {
+    const results = (await getDemandModelerData(source, date)).map(r => {
         let county_name = r['county_name'];
-        if (county_name === 'New York County'){
-            county_name= 'New York';
+        if (county_name === 'New York County') {
+            county_name = 'New York';
         }
-        let fatality_rate = (+r['fatality_rate']).toFixed(1)+"%";
-        return {...r, 
+        let fatality_rate = (+r['fatality_rate']).toFixed(1) + "%";
+        return {
+            ...r,
             fatality_rate
-            ,data_date: `${moment(r['data_date']).format("YYYY-MM-DD")} 12:00 EDT`
-            ,county_name}
+            , data_date: `${moment(r['data_date']).format("YYYY-MM-DD")} 12:00 EDT`
+            , county_name
+        }
     });
     const file = path.join(__dirname, '..', `covid19_county_${date}.csv`);
     const writeCleaned = writer({
